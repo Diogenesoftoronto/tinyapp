badbdb6d623a0da850cea2a4283daaf07a297419
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-07 10:59:16 -0500

    having problems getting the template usable the module isnt being recognized

--- express_server.js
+++ express_server.js
@@ -2,12 +2,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const morgan = require('morgan');
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+app.set("view engine", "ejs");
+const ejs = require('ejs');
+app.use(morgan('dev'));
 app.get("/", (req, res) => {
-    res.send("Hello!");
+    res.send("Hello!\n");
+});
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
 });
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
--- express_server.ts
+++ express_server.ts
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express()
-const PORT = 8080;
-
+const PORT = 8080
+const morgan = require('morgan');
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
+const ejs = require('ejs');
 
+app.set("view engine", "ejs");
+app.use(morgan('dev'));
 app.get("/", (req: any, res: any) => {
-  res.send("Hello!");
+  res.send("Hello!\n");
+});
+app.get("/urls", (req: any, res: any) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 app.get("/urls.json", (req: any, res: any) => {
   res.json(urlDatabase);
