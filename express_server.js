17c2ee65b63f8f1ef409197f9e948616547e6609
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-07 21:40:28 -0500

    finally got it to work

--- express_server.js
+++ express_server.js
@@ -1,18 +1,33 @@
 "use strict";
-const express = require('express');
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const PORT = 8080;
 const morgan = require('morgan');
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+// call the set up for th ejs from expressjs
 app.set("view engine", "ejs");
-const ejs = require('ejs');
+// call the morgan to log the request this is middleware it executes as a async function and is called before every route
 app.use(morgan('dev'));
-app.get("/", (req, res) => {
+// this is called every time some one goes to localhost:8080/
+app.get("/", (_req, res) => {
     res.send("Hello!\n");
 });
+// this is called everytime a short url is requested from urls
+app.get("/urls/:shortURL", (req, res) => {
+    const shortUrl = req.params.shortURL;
+    const templateVars = {
+        shortURL: req.params.shortURL, longURL: req.params.longURL
+    };
+    res.render("urls_show", templateVars);
+});
+//
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
--- express_server.ts
+++ express_server.ts
@@ -1,4 +1,4 @@
-const express = require('express');
+import express from "express";
 const app = express()
 const PORT = 8080
 const morgan = require('morgan');
@@ -6,21 +6,31 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
-const ejs = require('ejs');
 
+// call the set up for th ejs from expressjs
 app.set("view engine", "ejs");
+// call the morgan to log the request this is middleware it executes as a async function and is called before every route
 app.use(morgan('dev'));
-app.get("/", (req: any, res: any) => {
+// this is called every time some one goes to localhost:8080/
+app.get("/", (_req:  express.Request, res: express.Response) => {
   res.send("Hello!\n");
 });
-app.get("/urls", (req: any, res: any) => {
+// this is called everytime a short url is requested from urls
+app.get("/urls/:shortURL", (req:  express.Request, res: express.Response) => {
+  const shortUrl: string = req.params.shortURL;
+  const templateVars = { 
+    shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+//
+app.get("/urls", (req:  express.Request, res: express.Response) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-app.get("/urls.json", (req: any, res: any) => {
+app.get("/urls.json", (req:  express.Request, res: express.Response) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req: any, res: any) => {
+app.get("/hello", (req:  express.Request, res: express.Response) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.listen(PORT, () => {
