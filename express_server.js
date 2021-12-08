c75ac48037c7e24f03e824f0c74f2bbb2ada4996
diogenesoftoronto <diogenesoft@protonmail.com>
2021-12-08 00:09:23 -0500

    finished creating the random string generator function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const PORT = 8080;
 const morgan = require('morgan');
@@ -15,7 +16,9 @@ const urlDatabase = {
 app.set("view engine", "ejs");
 // call the morgan to log the request this is middleware it executes as a async function and is called before every route
 app.use(morgan('dev'));
-// this is called every time some one goes to localhost:8080/
+// make the data readable to humans from the buffer from form POST request for new urls
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+// this is called every time some one goes to localhost:PORT/
 app.get("/", (_req, res) => {
     res.render("frontpage");
 });
@@ -30,7 +33,6 @@ app.get("/urls/:shortURL", (req, res) => {
     };
     res.render("urls_show", templateVars);
 });
-
 // this is called when we want to look at all the urls in the database
 app.get("/urls", (_req, res) => {
     const templateVars = { urls: urlDatabase };
@@ -40,6 +42,19 @@ app.get("/urls", (_req, res) => {
 app.get("/urls.json", (_req, res) => {
     res.json(urlDatabase);
 });
+// this is called to recieve requests to the server
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
+// a function that generates a random string of 6 alphanumeric characters
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+function generateRandomString() {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    // definitely a better way to implement this but im lazy, i will just say that this is a way to program using the AHA method instead of DRY
+    const randomString = `${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}`;
+    return randomString;
+}
--- express_server.ts
+++ express_server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 const app = express()
 const PORT = 8080
 const morgan = require('morgan');
@@ -11,7 +12,10 @@ const urlDatabase = {
 app.set("view engine", "ejs");
 // call the morgan to log the request this is middleware it executes as a async function and is called before every route
 app.use(morgan('dev'));
-// this is called every time some one goes to localhost:8080/
+// make the data readable to humans from the buffer from form POST request for new urls
+app.use(bodyParser.urlencoded({extended: true}));
+
+// this is called every time some one goes to localhost:PORT/
 app.get("/", (_req:  express.Request, res: express.Response) => {
   res.render("frontpage");
 });
@@ -37,6 +41,20 @@ app.get("/urls", (_req:  express.Request, res: express.Response) => {
 app.get("/urls.json", (_req:  express.Request, res: express.Response) => {
   res.json(urlDatabase);
 });
+// this is called to recieve requests to the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+// a function that generates a random string of 6 alphanumeric characters
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+function generateRandomString() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  // definitely a better way to implement this but im lazy, i will just say that this is a way to program using the AHA method instead of DRY
+  const randomString = `${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}${characters.charAt(getRandomInt(0, characters.length))}`;
+  return randomString;
+}
