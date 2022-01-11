8769288b288d5a34c561d02e0653d223c89ab692
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-10 19:18:34 -0500

    added comment

--- generate-random-string.ts
+++ generate-random-string.ts
@@ -1,6 +1,7 @@
 
 // a function that generates a random string of 6 alphanumeric characters
 function generateRandomString() {
+  // the suggestion to add a for loop here is confusing. This is a single line of code using functions already built into js. I will ignore this suggestion for now.
   return Math.random().toString(20).substr(2, 6);
 }
 export {generateRandomString};
