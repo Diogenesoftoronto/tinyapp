0a84da634eb3bcf7dd48f222a3d2f1329df42a53
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-10 19:24:46 -0500

    deleted js files

--- generate-random-string.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateRandomString = void 0;
-// a function that generates a random string of 6 alphanumeric characters
-function generateRandomString() {
-    return Math.random().toString(20).substr(2, 6);
-}
-exports.generateRandomString = generateRandomString;
