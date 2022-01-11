553434db7e72f62bf2fb68065fdff666ef1cef83
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-10 19:20:14 -0500

    removed any types and replaced with specific type checking also added new function to check if a url is in the database

--- classes.ts
+++ classes.ts
@@ -1,39 +1,49 @@
 import bcrypt from "bcryptjs";
+import { Session } from "express-session";
+interface url {
+  [shortUrl: string]: string
+};
+type urlArray = [url?];
 class User {
-  [x: string]: any;
-  constructor(username: any){
+  username: string;
+  urls: urlArray;
+  password: string;
+  email: string;
+  urlsDB: {url?: url} | any;
+  session: Session;
+
+
+  constructor(username: string){
     this.username = username;
-    this.urls = [];
     this.password;
     this.email;
     this.session;
     this.urlsDB = {};
   }
   get getUsername() {
-    return this.username;
+    if (typeof this.username === 'string') {
+      return this.username;
+
+    }
   }
-  set setPassword (password: any) {
+  set setPassword (password: string) {
     this.password = password;
   }
   get getPassword() {
     return this.password;
   }
-  set setEmail (email: any) {
+  set setEmail (email: string) {
     this.email = email;
   }
   get getEmail() {
     return this.email;
   }
-  set setUrls (url: object) {
-    this.urls.push(url);
+  set setUrls (url: url) {
+    Object.assign(this.urlsDB, url);
   }
   get getUrls() {
-    this.urls.forEach((url: any) => {
-      const key = Object.keys(url)[0];
-      this.urlsDB[key] = url[key];
-    });
-
-    return this.urlsDB;
+    // implemented a hashtable to get and set values, eliminates need for array also faster
+    return this.urlsDB;    
   }
   set setSession (session: any) {
     this.session = session;
@@ -41,15 +51,11 @@ class User {
   get getSession() {
     return this.session;
   }
-  set encryptPassword (password: any) {
+  set encryptPassword (password: string) {
     this.password = bcrypt.hashSync(password, 10);
   }
   checkPassword (password: any) {
     return bcrypt.compareSync(password, this.password);
-  }
-  deleteUrls (shorturl: any) {
-    // first delete the url in the urlDB then delete the index element of the urls array
-    
   }
 
 }
@@ -70,21 +76,23 @@ class User {
 // user2.setEmail = 'superman@google.com';
 // user3.setUrlS = { "s8h76q": "https://www.google.com"}
 // set session for the user will be a cookie!
+interface users {
+  [username: string]: User;
+}
 class Database {
-  [x: string]: any;
+  users: users;
   constructor() {
-    this.Database = {};
-    this.Database.users = {};
+    this.users = {};
   }
   get usersInDB() {
     
-    return this.Database.users;
+    return this.users;
   }
   userbyUsername(username: any) {
-    return this.Database.users[username];
+    return this.users[username];
   }
   set setUser (user: User) {
-    // if you try to enter an empty user into the database, it will throw an error!
+    // if you try to enter a user with missing information, it will throw an error!
     if (user.getUsername === '') {
       throw new Error('username cannot be empty');
     }
@@ -94,14 +102,14 @@ class Database {
     if (user.getEmail === '') {
       throw new Error('email cannot be empty');
     }
-    this.Database.users[user.getUsername] = user;
+    this.users[user.getUsername] = user;
   }
-  isUsernameInDB(username: any) {
+  isUsernameInDB(username: string) {
     // drifyied!
     let result = false;
 
     try {
-     if (this.Database.users[username].username === username) {
+     if (this.users[username].username === username) {
      result = true;
       }
     } catch (TypeError) {
@@ -109,33 +117,32 @@ class Database {
     }
     return result;
   };
-  isPassInDB(username: any, password: any) {
+  isPassInDB(username: string, password: string) {
     let result = false;
-    if (this.Database.users[username].password === password) {
+    if (this.users[username].password === password) {
       result = true;
     }
     return result;
   }
-  isEmailInDB(username: any, email: any) {
+  isEmailInDB(username: string, email: string) {
     let result = false;
-    if (this.Database.users[username].email === email) {
+    if (this.users[username].email === email) {
       result = true;
     }
     return result;
   }
-  isSessionInDB(username: any, session: any) {
+  isSessionInDB(username: string, session: Session) {
     let result = false;
-    if (this.Database.users[username].session === session) {
+    if (this.users[username].session === session) {
       result = true;
     }
     return result;
   }
-  isUserInfoInDB(username: any, email: any, password: any) {
+  isUserInfoInDB(username: string, email: string, password: string) {
     // drifyied!
-    console.log("database user", this.Database.users[username])
     let result = false;
     try {
-      if (this.Database.users[username].email === email && this.Database.users[username].password === password) {
+      if (this.users[username].email === email && this.users[username].password === password) {
         
         result = true;
       }
@@ -145,6 +152,20 @@ class Database {
     }
 
     return result;
+  }
+  isUrlInDB (shortUrl: string): Array<Boolean | string> {
+    // loop through users
+    for (const username in this.users) {
+      // assign the current user to a variable
+        const user = this.userbyUsername(username);
+        // assign the urls of the current user to a variable
+        const urls = user.getUrls;
+        // check whether the url is in the users urls
+        if (urls.hasOwnProperty(shortUrl)) {
+          return [true, urls[shortUrl]];
+        }
+    }
+    return [false];
   }
 
 
